77fadb8a86cd1a444b892aed77e2c102f3437887
Uday Kumar <uday256071@gmail.com>
2026-06-09 00:11:16 +0530

    Added prisma setup for client and migrations

--- calendely-backend/src/server.ts
+++ calendely-backend/src/server.ts
@@ -1,13 +1,15 @@
-import { app } from './app.js';
-import { connectDatabase } from './config/database.js';
-import { PORT } from './config/env.js';
+import { app } from "./app.js";
+import { connectDatabase } from "./config/database.js";
+import { PORT } from "./config/env.js";
 
 export async function startServer() {
-    await connectDatabase();
-    app.listen(PORT, () => {
-        console.log(`[server]: Running on port ${PORT}`)
-    });
+  await connectDatabase();
+  app.listen(PORT, () => {
+    console.log(`[server]: Running on port ${PORT}`);
+  });
 }
 
-
-startServer();
+startServer().catch((err) => {
+  console.log("[Server]: Failed to start", err);
+  process.exit(1);
+});
